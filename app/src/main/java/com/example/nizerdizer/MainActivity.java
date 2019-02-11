c0be90b30abe9dd6b0ffafc1a7cb5e794b926163
Andreas Orler <andreas.orler@students.fhnw.ch>
2019-02-11 21:39:48 +0100

    dice logic added

--- app/src/main/java/com/example/nizerdizer/MainActivity.java
+++ app/src/main/java/com/example/nizerdizer/MainActivity.java
@@ -8,12 +8,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Field to hold the roll result text
+    TextView rollResult;
+
+    //Fielt to hold the roll button
+    Button rollButton;
+
     //Field to hold the score
     int score;
+
+    //Field to hold random number generator
+    Random rand;
+
+    //Field to hold the die value
+    int die1;
+    int die2;
+    int die3;
+
+    //ArrayList to hold all three dice values
+    ArrayList<Integer> dice;
 
 
     @Override
@@ -36,7 +58,38 @@ public class MainActivity extends AppCompatActivity {
         score = 0;
 
         //Create greeting
-        Toast.makeText(getApplicationContext(),"Welcome to NizerDizer",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Welcome to NizerDizer", Toast.LENGTH_SHORT).show();
+
+        //Link instance to widgets in the activity
+        rollResult = (TextView) findViewById(R.id.rollResult);
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        //Initialize random number generator
+        rand = new Random();
+
+        //Create ArrayList container for the dice values
+        dice = new ArrayList<>();
+
+    }
+
+    public void rollDice(View v) {
+        //Roll dice
+        die1 = rand.nextInt(6) + 1;
+        die2 = rand.nextInt(6) + 1;
+        die3 = rand.nextInt(6) + 1;
+
+        //Set dice values into ArrayList
+        dice.clear();
+        dice.add(die1);
+        dice.add(die2);
+        dice.add(die3);
+
+        //Build message with the result
+        String msg = "You rolled a " + die1 + ", a " + die2 + ", a " + die3;
+
+        //Update the app to display the result message
+        rollResult.setText(msg);
+
     }
 
     @Override
